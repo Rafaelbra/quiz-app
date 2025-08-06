a9e4075d9e34c324df26f7549770e172fb30e831
Rafael Braga <rafaelbra149@gmail.com>
2025-08-06 15:46:55 -0400

    Tracking user answers

--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const Quiz = () => {
     const questionBank = [
@@ -23,14 +24,28 @@ const Quiz = () => {
         },
     ];
 
+    const initialAnswers = [null, null, null];
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const [userAnswers, setUserAnswers] = useState(initialAnswers);
+
+    function handleSelectedOption(option) {
+        setOptionSelected(option)
+    }
+
   return (
     <div>
         <h2>Question 1</h2>
-        <p className="question">{questionBank[0].question}</p>
+        <p className="question">{questionBank[currentQuestion].question}</p>
 
-        {questionBank[0].options.map((option) => (
-            <butto className='option'>{option}</butto>
+        {questionBank[currentQuestion].options.map((option) => (
+            <button className='option' onClick={() => handleSelectedOption(option)}>{option}</button>
         ))}
+
+        <div className="nav-buttons">
+            <button>Previous</button>
+            <button>Next</button>
+        </div>
         
     </div>
   )
